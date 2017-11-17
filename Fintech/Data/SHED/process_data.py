c259b5883d5ead587fa10c296ddabc17f8b69512
DANIEL SHENFELD <daniel@DANIELs-MacBook-Pro-2.local>
2017-11-17 10:21:16 -0500

    update mapper

--- Fintech/Data/SHED/process_data.py
+++ Fintech/Data/SHED/process_data.py
@@ -35,13 +35,13 @@ from collections import OrderedDict
 
 import pandas as pd
 
-SOURCE_SHED_DATAFILE = 'SHED_2016_Public_Data.csv'
+SOURCE_SHED_DATAFILE = 'SHED_2016_Public_Data.csv.gz'
 MAPPER_FILE = 'data_mapper.json'
 
 YES_NO_VALUE_MAP = {
-    "Yes": True,
-    "No": False,
-    "Refused": False,  # Or None?
+    u"Yes": True,
+    u"No": False,
+    u"Refused": False,  # Or None?
     None: False,
 }
 
@@ -49,10 +49,10 @@ YES_NO_VALUE_MAP = {
 class SHEDDataMapper(object):
     """ Mapper class for SHED data """
 
-    def __init__(self, source_df=None, mapper=None):
+    def __init__(self, src_df=None, mapper=None):
         "Read source dataframe and mapper, create an empty target dataframe with the same index"
-        self.source_df = source_df or pd.read_csv(SOURCE_SHED_DATAFILE)
-        self.target_df = pd.DataFrame(index=self.source_df.index)
+        self.src_df = src_df or pd.read_csv(SOURCE_SHED_DATAFILE, compression='gzip')
+        self.target_df = pd.DataFrame(index=self.src_df.index)
         self.mapper = mapper
         if self.mapper is None:
             with open(MAPPER_FILE) as mapper_json:
@@ -61,7 +61,9 @@ class SHEDDataMapper(object):
     def map_dataframe(self):
         """ Map all the columns in the mapper """
         for src_col, data in self.mapper.items():
-            column_type = self.validate_mapping_data(data)
+            if data.get('ignore'):
+                continue
+            column_type = self.validate_mapping_data(src_col, data)
             if column_type == 'multiple_choice':
                 self.map_multiple_choice_column(src_col, data)
             elif column_type == 'single_choice':
@@ -69,7 +71,7 @@ class SHEDDataMapper(object):
             else:
                 raise NotImplementedError
 
-    def validate_mapping_data(self, data):
+    def validate_mapping_data(self, src_col, data):
         """Validate column mapping data. Column is either multiple choice, in which case data must
         include 'prefix' and 'columns', or single type in which case it must include 'name' and 
         'values' """
@@ -81,12 +83,15 @@ class SHEDDataMapper(object):
             return 'multiple_choice'
         if is_single_choice:
             return 'single_choice'
-        raise ValueError("Cannot detect column type")
+        raise ValueError("{}: Cannot detect column type".format(src_col))
 
-    def assert_all_values_included(src_col, value_map):
-        """ Assert that all the values for the source column are included in the value map. """
-        src_vals = set(self.source_df[src_col].dropna().values)
-        assert src_vals.issubset(set(value_map.keys()))
+    def validate_all_values_included(self, src_col, value_map):
+        """ validate that all the values for the source column are included in the value map. """
+        # Pandas doesn't play nice with unicode. Decode explicitly.
+        unicode_values = [v.decode('utf-8') for v in self.src_df[src_col].value_counts().index]
+        src_vals = set(unicode_values)
+        if not src_vals.issubset(set(value_map.keys())):
+            raise ValueError("{}: Value not included in map".format(src_col))
 
     def map_multiple_choice_column(self, src_col_prefix, mapper):
         "Map a multiple choice column"
@@ -94,13 +99,13 @@ class SHEDDataMapper(object):
         column_name_map = OrderedDict([
             ('{}_{}'.format(src_col_prefix, src_col_suffix),
              '{}_{}'.format(mapper['prefix'], target_col_suffix))
-            for soure_col_suffix, target_col_suffix in zip(abc[:len(mapper['columns'])], mapper['columns'])
+            for src_col_suffix, target_col_suffix in zip(abc[:len(mapper['columns'])], mapper['columns'])
             ])
         for src_col, target_col in column_name_map.items():
-            self.assert_all_values_included(src_col, YES_NO_VALUE_MAP)
+            self.validate_all_values_included(src_col, YES_NO_VALUE_MAP)
             self.target_df[target_col] = self.src_df[src_col].map(YES_NO_VALUE_MAP)
 
-    def process_numeric_column_map(self, src_col):
+    def process_numeric_column(self, src_col):
         "Process a numeric column: set all non-numeric values to None"
         def float_or_none(s):
             try:
@@ -114,24 +119,28 @@ class SHEDDataMapper(object):
         "Map a single question column"
         values = mapper['values']
         target_col = mapper['name']
+        if values is None:  # Copy column as is
+            self.target_df[target_col] = self.src_df[src_col]
+            return
+
         if values == 'yes_no':
             values = YES_NO_VALUE_MAP
         if isinstance(values, dict):
-            self.assert_all_values_included(src_col, values)
-            self.target_df[target_col] = self.src_df[source_col].map(values)
+            self.validate_all_values_included(src_col, values)
+            self.target_df[target_col] = self.src_df[src_col].map(values)
             return
+
         if values == 'numeric':
             self.target_df[target_col] = self.process_numeric_column(src_col)
             return
 
-        raise ValueError("Values must be a dict, numeric, or yes_no")
+        raise ValueError("{}: Values must be a dict, numeric, or yes_no".format(src_col))
 
 
 def main():
     mapper = SHEDDataMapper()
     mapper.map_dataframe()
-    print pd.to_csv(mapper.target_df)
-
+    print mapper.target_df.to_csv(index=False)
 
 if __name__ == '__main__':
     main()
